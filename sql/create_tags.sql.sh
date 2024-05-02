2cc87bd750a60ebb00e2053b22e88209b5c26d68
a-givertzman <a.givertzman@icloud.com>
2024-05-02 21:12:49 +0300

    Build deb | build

--- sql/create_tags.sql.sh
+++ sql/create_tags.sql.sh
@@ -2,22 +2,19 @@
 #    PROCESS TAG
 #
 read -r -d '' sql << EOF
-
-    \\connect $db;
-
     do \$\$
     begin
         if not exists (SELECT 1 FROM pg_type WHERE typname = 'tag_type_enum') THEN
-            create type tag_type_enum as enum('Bool','Int','UInt','DInt','Word','LInt','Real','Time','Date_And_Time');
+            create type $db.public.tag_type_enum as enum('Bool','Int','UInt','DInt','Word','LInt','Real','Time','Date_And_Time');
         end if;
     end
     \$\$;
-    create table if not exists tags (
+    create table if not exists $db.public.tags (
         id 		serial not null,
         type      tag_type_enum not null,
         name      varchar(255) not null unique,
         description   varchar(255) not null DEFAULT '',
         PRIMARY KEY (id)
     );
-    comment on table tags is 'Tag dictionary';
+    comment on table $db.public.tags is 'Tag dictionary';
 EOF

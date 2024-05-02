6e8a1d69b18b319a759edc6552c163b08b4c1ed8
a-givertzman <a.givertzman@icloud.com>
2024-05-02 20:11:11 +0300

    Build deb | build

--- sql/create_tags.sql
+++ sql/create_tags.sql
@@ -11,9 +11,9 @@ read -r -d '' sql << EOF
     \$\$;
     create table if not exists tags (
         id 		serial not null,
-        type      tag_type_enum not null,                 --COMMENT 'S7DataType(bool, int, uInt, dInt, word, lInt, real, time, dateAndTime)',
-        name      varchar(255) not null unique,	        --COMMENT 'Имя тэга в системе',
-        description   varchar(255) not null DEFAULT '',   --COMMENT 'Дополнительная информация о тэге',
+        type      tag_type_enum not null,
+        name      varchar(255) not null unique,
+        description   varchar(255) not null DEFAULT '',
         PRIMARY KEY (id)
     );
 EOF

c8af1f61c3de5c57fa819239567e31b8ff1da9e6
a-givertzman <a.givertzman@icloud.com>
2024-05-02 19:58:50 +0300

    Build deb | build

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,6 +1,13 @@
 read -r -d '' sql << EOF
-    if not exists (SELECT 1 FROM pg_database WHERE datname = '$db') THEN
-        CREATE DATABASE $db;
-        GRANT ALL PRIVILEGES ON DATABASE $db TO $user;
-    end if;
+    SELECT 'CREATE DATABASE $db'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec;
+    do \$\$
+    begin
+        if exists (SELECT 1 FROM pg_database WHERE datname = '$db') THEN
+            if exists (SELECT 1 FROM pg_roles WHERE rolname='$user') then
+                GRANT ALL PRIVILEGES ON DATABASE $db TO $user;
+            end if;
+        end if;
+    end
+    \$\$;
 EOF

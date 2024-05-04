220aa3dbc2079a104e1350eaa88544b258ef5caf
a-givertzman <a.givertzman@icloud.com>
2024-05-04 16:50:35 +0300

    Build deb | build

--- sql/drop_event.sql
+++ sql/drop_event.sql
@@ -6,7 +6,7 @@
     drop function if exists event_counter_dec();
     drop trigger if exists event_insert_trigger on event;
     drop function if exists event_counter_inc();
-    drop function if exists event_check_for_purge()
+    drop function if exists event_check_for_purge();
     drop function if exists event_purge_records();
     drop table if exists event_utils;
     drop table if exists event;
